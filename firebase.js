6ed3d32961c4e51f6e809837acf29532880cdd3f
Attah David <jocheattahdavid@gmail.com>
2021-02-05 19:03:46 +0100

    completed register

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDWh_LQIPMAV2Ye5HK70m52czx8AJcnG5Y",
+    authDomain: "signal-native-7ff66.firebaseapp.com",
+    projectId: "signal-native-7ff66",
+    storageBucket: "signal-native-7ff66.appspot.com",
+    messagingSenderId: "770734863887",
+    appId: "1:770734863887:web:9407bee2f18da405fe557a"
+  };
